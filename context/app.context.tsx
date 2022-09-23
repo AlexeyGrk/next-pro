5d3281feffc540b3a7639b6aeb55e6517983fa67
AlexeyGrk <alexgmirko@gmail.com>
2022-09-23 12:04:28 +0300

    small bugfix

--- context/app.context.tsx
+++ context/app.context.tsx
@@ -7,7 +7,7 @@ export interface IAppContext {
   firstCategory:TopLevelCategory,
   setMenu?: (newMenu:MenuItem[])=> void;
 }
-//helo
+
 export const AppContext = createContext<IAppContext>({ menu:[],firstCategory:TopLevelCategory.Courses});
 
 
