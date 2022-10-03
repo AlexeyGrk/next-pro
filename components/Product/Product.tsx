036fc7757208b7a5f3b856aeb059cad5b331a775
AlexeyGrk <alexgmirko@gmail.com>
2022-10-03 11:49:46 +0300

    add Product components and small changes for TopPage

---
+++ components/Product/Product.tsx
@@ -0,0 +1,11 @@
+import {ProductProps} from './Product.props';
+import cn from 'classnames';
+import styles from './Product.module.css';
+
+export const Product = ({product,className, ...props}:ProductProps):JSX.Element =>{
+ return (
+ <div className={cn(className,styles.productWrapper)} {...props}>
+   {product.title}
+ </div>
+ );
+};
