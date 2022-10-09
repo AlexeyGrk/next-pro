852a7106a12660516b341ff9dace44d63d85334b
AlexeyGrk <alexgmirko@gmail.com>
2022-10-09 22:47:16 +0300

    draft for main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,17 @@
 import type { GetStaticProps } from 'next';
 import axios from 'axios';
-import { Htag, Button, Pbig, Tag, Rating} from '../components';
+import { Htag} from '../components';
 import { useState } from 'react';
 import { withLayout } from '../layout/Layout';
 import { MenuItem } from '../interfaces/menu.interface';
-import { TextArea } from '../components/TextArea/TextArea';
 import { API } from '../heplers/api';
 
 
 const Home= ({menu}:HomeProps):JSX.Element => {
-  const [rating,setRating] = useState<number>(4);
 
   return (
     <>
-      <Htag tag='h1'>Hello</Htag>
-      <Button arrow={'right'} appearance={'ghost'} > Click </Button>
-      <Pbig size={'s'}> Small</Pbig>
-      <Pbig > Medium</Pbig>
-      <Pbig size={'l'}> Large</Pbig>
-      <Tag size={'s'} color={'green'}>
-        small
-      </Tag>
-      <Tag size={'m'} color={'primary'}>
-       medium
-      </Tag>
-      <Rating rating={rating} isEditable={true} setRating={setRating}/>
-      {/*<Input placeholder={'Имя'}/>*/}
-      <TextArea/>
+      <Htag tag='h1'>Выбери свое будущее!</Htag>
 
     </>
   );
