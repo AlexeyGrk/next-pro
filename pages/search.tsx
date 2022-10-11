ffdb3178f8b944cf19a587b730a118c8b2e06cb4
AlexeyGrk <alexgmirko@gmail.com>
2022-10-11 18:17:10 +0300

    add responsive for start page

--- pages/search.tsx
+++ pages/search.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { withLayout } from '../layout/Layout';
 import { MenuItem } from '../interfaces/menu.interface';
 import { API } from '../heplers/api';
+import { ProductModel } from '../interfaces/product.interface';
 
 
 const Search = ():JSX.Element => {
@@ -24,6 +25,7 @@ export const getStaticProps: GetStaticProps<HomeProps> = async  () => {
   const {data:menu} = await axios.post<MenuItem[]>(API.topPage.find,{
     firstCategory
   });
+
   return{
     props:{
       menu,
