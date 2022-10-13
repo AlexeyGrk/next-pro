d1decc34714e861c88295064dbcd89406bb1ba0d
AlexeyGrk <alexgmirko@gmail.com>
2022-10-13 16:59:54 +0300

    bugfix/menu and logo link

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -4,7 +4,6 @@ import styles from './Sidebar.module.css';
 import {Menu} from '../Menu/Menu';
 import Logo from '../Logo/logo.svg';
 import {Search} from '../../components';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 
@@ -13,7 +12,7 @@ import { useRouter } from 'next/router';
 export const Sidebar = ({className,...props}:SidebarProps):JSX.Element =>{
   const router = useRouter();
  return (
-   <div {...props} onClick={()=>router.push('/')} className={cn(className,styles.sidebar)}>
+   <div {...props} onClick={()=>router.push('/courses')} className={cn(className,styles.sidebar)}>
        <Logo   className={styles.logo}/>
      <Search/>
      <Menu />
