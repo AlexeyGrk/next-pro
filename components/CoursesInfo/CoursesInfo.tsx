3dbeefe202d3a218e20a50f14c0d9abffc70e17d
AlexeyGrk <alexgmirko@gmail.com>
2022-10-16 20:30:40 +0300

    add CoursesInfo cards for popular courses

--- components/CoursesInfo/CoursesInfo.tsx
+++ components/CoursesInfo/CoursesInfo.tsx
@@ -6,21 +6,21 @@ import { Card } from '../Card/Card';
 import { motion } from 'framer-motion';
 import { firstLevelMenu } from '../../heplers/helpers';
 import { useRouter } from 'next/router';
+import cn from 'classnames';
+import { Htag } from '../Htag/Htag';
 
 
 export const CoursesInfo = ({menu,products}:CoursesInfoProps):JSX.Element =>{
   const router = useRouter();
   const buildSecondLevel = (products: any,secondCategory:string) => {
     return (
-      <div>
       <ul className={styles.secondBlock}>
         {products.map((product:any)=>{
           if(product._id.secondCategory===secondCategory){
-            return (<li key={secondCategory}>{product.pages.slice(0,3).map((page)=><a href={`${router?.query ? router?.query?.type : ''}/${page.alias}`}>{page.title}</a>)}</li>);
+            return (<li className={styles.secondBlockItem} key={secondCategory}>{product.pages.slice(0,3).map((page)=><a  className={styles.secondBlockItemLink}  href={`${router?.query ? router?.query?.type : ''}/${page.alias}`}>{page.title}</a>)}</li>);
           }
         })}
       </ul>
-      </div>
     );
 
   };
@@ -34,15 +34,21 @@ export const CoursesInfo = ({menu,products}:CoursesInfoProps):JSX.Element =>{
           <Dots className={styles.dots}/>
         </div>
       </a>
+      <h2 >Популярные курсы по направлениям</h2>
 
       <ul className={styles.cardList}>
-        {menu && menu.map(i=>(
+        {menu && menu.map((i,idx)=>(
           <motion.div key={i._id.secondCategory} whileHover={{ scale: 1.1 }}
                       whileTap={{ scale: 0.8 }}
                       // style={{ x: 100 }}
           >
-            <Card className={styles.card} color={'blue'} key={i._id.secondCategory}>
-              {i._id.secondCategory}
+            <Card className={cn(styles.card,{
+              [styles.odd]:idx%2===0,
+              [styles.even]:idx%2!==0
+            })} color={'blue'} key={i._id.secondCategory}>
+             <div className={styles.title}>
+               {i._id.secondCategory}
+             </div>
                 {buildSecondLevel(products,i._id.secondCategory)}
             </Card>
           </motion.div>
