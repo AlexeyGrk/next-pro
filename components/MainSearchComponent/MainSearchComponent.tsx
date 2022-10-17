76cb401a814c60660cad2485d7c84d54d542f884
AlexeyGrk <alexgmirko@gmail.com>
2022-10-17 19:23:11 +0300

    add not-found page for search

--- components/MainSearchComponent/MainSearchComponent.tsx
+++ components/MainSearchComponent/MainSearchComponent.tsx
@@ -4,27 +4,32 @@ import { arraySearch } from '../../heplers/helpers';
 import { useRouter } from 'next/router';
 import { Htag } from '../Htag/Htag';
 import { Devider } from '../Devider/Devider';
+import NotFound from '../../heplers/icons/not-found.svg';
+import { Pbig } from '../Pbig/Pbig';
 
 
+export const MainSearchComponent = ({products}: MainSearchComponentProps): JSX.Element => {
+  const router = useRouter();
+  const result = router?.query?.q && arraySearch(products.map(i => i.pages).flat(1), router?.query?.q);
 
-export const MainSearchComponent = ({products}:MainSearchComponentProps):JSX.Element =>{
- const router = useRouter();
- const result = router?.query?.q && arraySearch(products.map(i=>i.pages).flat(1),router?.query?.q);
+  return (
+    <div className={styles.wrapper}>
+      <Htag className={styles.heading} tag={'h2'}>Вот что мы нашли по вашему запросу : <span className={styles.searchText}>{`${router.query ? router?.query.q : 'Загружаем...'}`}</span></Htag>
+      <div className={styles.list}>
+        {result && result.length ? result.map((i) =>
+          <div className={styles.listItem} key={i._id}>
+            <a className={styles.listLink} href={`/courses/${i.alias}`}>{i.title}</a>
+            <Devider/>
+          </div>
+        ) : <div className={styles.notFoundContainer}>
+          <span >По вашему запросу ничего не найдено</span>
+          <span >Попробуйте, еще раз (например: Аналитика) </span>
+         <NotFound className={styles.notFoundIcon} />
+        </div>}
 
- return (
-  <div className={styles.wrapper}>
-   <Htag className={styles.heading}  tag={'h2'}>Вот что мы нашли по вашему запросу : <span className={styles.searchText}>{`${router.query ? router?.query.q : 'Загружаем...'}`}</span></Htag>
-   <div className={styles.list}>
-    {result && result.length ? result.map((i)=>
-      <div className={styles.listItem} key={i._id}>
-       <a className={styles.listLink} href={`/courses/${i.alias}`}>{i.title}</a>
-       <Devider />
       </div>
-    ):<div>Not found</div>}
-
-   </div>
 
 
-  </div>
- );
+    </div>
+  );
 };
